88b7a6c7dee852ef1905f0cb6328366c4699763c
Alexandre de Alencar <alexandre.b.alencar@accenture.com>
2022-11-17 13:57:31 -0300

    perfumaria

--- pipeline/pipeline.groovy
+++ pipeline/pipeline.groovy
@@ -1,18 +1,26 @@
 node('docker-node') {
   def buildNumber = currentBuild.number
-  def name_img = "${env.JOB_NAME}:${buildNumber}"
+  def name_img = "${env.JOB_NAME}"
   def image
-  stage('Prepare') {
+  def version = "${buildNumber}"
+  stage('Checkout Repository') {
     deleteDir()
     checkout scm
   }
-  stage('Build'){
+  stage('Build Image'){
       image = docker.build("$name_img")
     }
-  stage('Push'){
+  stage('Push Image'){
     docker.withRegistry('http://20.231.125.187:8182', 'nexus') {
-      image.push()
-  }  
+      image.push("$version")
+      image.push("latest")
+    }  
+  }
+  stage('Delete Image'){
+    def imgrm
+    imgrm = sh(script: "docker images $name_img:latest -q", returnStdout: true).trim()
+    println imgrm
+    sh("docker rmi -f $imgrm")
   }
   stage('Deploy'){
     withCredentials([file(credentialsId: 'kubeconfig', variable: 'SECRET_FILE')]) {
