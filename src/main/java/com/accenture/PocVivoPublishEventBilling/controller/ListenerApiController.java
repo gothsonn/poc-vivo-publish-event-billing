1ec31bbe03240425e3ee5c9b43360861aabb1c6b
Rafael Freitas <gothsonn@gmail.com>
2022-12-15 17:31:13 -0300

    Commit for start publish messages

--- src/main/java/com/accenture/PocVivoPublishEventBilling/controller/ListenerApiController.java
+++ src/main/java/com/accenture/PocVivoPublishEventBilling/controller/ListenerApiController.java
@@ -36,48 +36,6 @@ public class ListenerApiController implements ListenerApi {
         this.topicProducer = topicProducer;
     }
 
-    public ResponseEntity<EventSubscription> listenToBillingAccountAttributeValueChangeEvent(@ApiParam(value = "The event data" ,required=true )  @Valid @RequestBody BillingAccountAttributeValueChangeEvent data) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<EventSubscription>(objectMapper.readValue("{\"empty\": false}", EventSubscription.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<EventSubscription>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<EventSubscription>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
-    public ResponseEntity<EventSubscription> listenToBillingAccountStateChangeEvent(@ApiParam(value = "The event data" ,required=true )  @Valid @RequestBody BillingAccountStateChangeEvent data) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<EventSubscription>(objectMapper.readValue("{\"empty\": false}", EventSubscription.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<EventSubscription>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<EventSubscription>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
-    public ResponseEntity<EventSubscription> listenToFinancialAccountAttributeValueChangeEvent(@ApiParam(value = "The event data" ,required=true )  @Valid @RequestBody FinancialAccountAttributeValueChangeEvent data) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<EventSubscription>(objectMapper.readValue("{\"empty\": false}", EventSubscription.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<EventSubscription>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<EventSubscription>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
     public ResponseEntity<EventSubscription> listenToFinancialAccountCreateEvent(@ApiParam(value = "The event data" ,required=true )  @Valid @RequestBody FinancialAccountCreateEvent data) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
@@ -118,61 +76,4 @@ public class ListenerApiController implements ListenerApi {
 
         return new ResponseEntity<EventSubscription>(HttpStatus.NOT_IMPLEMENTED);
     }
-
-    public ResponseEntity<EventSubscription> listenToPartyAccountAttributeValueChangeEvent(@ApiParam(value = "The event data" ,required=true )  @Valid @RequestBody PartyAccountAttributeValueChangeEvent data) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<EventSubscription>(objectMapper.readValue("{\"empty\": false}", EventSubscription.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<EventSubscription>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<EventSubscription>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
-    public ResponseEntity<EventSubscription> listenToPartyAccountStateChangeEvent(@ApiParam(value = "The event data" ,required=true )  @Valid @RequestBody PartyAccountStateChangeEvent data) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<EventSubscription>(objectMapper.readValue("{\"empty\": false}", EventSubscription.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<EventSubscription>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<EventSubscription>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
-    public ResponseEntity<EventSubscription> listenToSettlementAccountAttributeValueChangeEvent(@ApiParam(value = "The event data" ,required=true )  @Valid @RequestBody SettlementAccountAttributeValueChangeEvent data) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<EventSubscription>(objectMapper.readValue("{\"empty\": false}", EventSubscription.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<EventSubscription>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<EventSubscription>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
-    public ResponseEntity<EventSubscription> listenToSettlementAccountStateChangeEvent(@ApiParam(value = "The event data" ,required=true )  @Valid @RequestBody SettlementAccountStateChangeEvent data) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<EventSubscription>(objectMapper.readValue("{\"empty\": false}", EventSubscription.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<EventSubscription>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<EventSubscription>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
 }
